7c5898011957f4bb35945aecb33a7965f5058933
isadahmerb <dahmerisadora@gmail.com>
2025-08-26 16:41:06 -0300

    ADD projeto0

---
+++ src/main/java/httpserver/httpserver.java
@@ -0,0 +1,15 @@
+package httpserver;
+
+/**
+ * Driver Class for the http server
+ */
+
+public class httpserver {
+
+    public static void main(String[] args) {
+
+        System.out.println("SERVER STARTING...");
+    }
+
+}
+
